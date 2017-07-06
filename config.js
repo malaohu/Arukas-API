3da858e8820744a802ccec003fbab10f3364b0f9
root <root@li764-100.(none)>
2017-07-06 15:50:24 +0800

    更新配置文件

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
-//config file
 var config = {
-    debug   :   true,
-    port    :   13999,
+    token       : '', 
+    secret      : '',
+    is_cron     : true,
+    
     cache   :   true,//启动缓存
     cache_timeout: 1000 * 60 * 10,//缓存时间单位毫秒 
 
-    token       : '8feb8511-fd02-47e0-af62-29885692eb10', 
-    secret      : 'oGLg8Qgk6UdQOZs1fnYZR9hxGKETjD7t1TxcDSb2o799fxREdMcegyuVrlpLp10A',
-    is_cron     : true
 }
 module.exports = config;
