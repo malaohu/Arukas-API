f62ad9785052a59ae477bdfb0db9402da2abc387
malaohu <re_ming.ma@hotmail.com>
2017-07-06 15:57:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
-    token       : '', 
-    secret      : '',
-    is_cron     : true,
+    token       : 'yourtoken', 
+    secret      : 'yoursecret',
+    is_cron     : yourcron,
     
     cache   :   true,//启动缓存
     cache_timeout: 1000 * 60 * 10,//缓存时间单位毫秒 
